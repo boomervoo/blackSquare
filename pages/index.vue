aaf9847d185808f02063d6b4229cd71d29c94d61
boomervoo <boomervoo@gmail.com>
2025-03-11 10:53:30 +0700

    init commit
    # Conflicts:
    #	README.md

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex items-center justify-center h-screen">
+  <div class="flex items-center justify-center w-80 h-80 bg-black">
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
